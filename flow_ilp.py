cc7fdf62506224aa995ecd158964b0a49b3e0b01
hugocarmaga <hugocarmaga@hotmail.com>
2024-06-20 11:33:33 +0200

    add free_both for nodes without edges

--- flow_ilp.py
+++ flow_ilp.py
@@ -31,6 +31,7 @@ def ilp(nodes, edges, coverages, alpha, beta, rlen_params, outfile, source_prob 
         double_sides = defaultdict()
         free_left_side = defaultdict()
         free_right_side = defaultdict()
+        free_both = defaultdict()
 
         # Super-edges flow on the two sides of the node (I don't care if it's for the supersource or the supersink, only the node side matters)
         source_left = {node: model.addVar(vtype=GRB.INTEGER, lb = 0,  name = "source_left_"+node) for node in covered_nodes}
@@ -71,8 +72,10 @@ def ilp(nodes, edges, coverages, alpha, beta, rlen_params, outfile, source_prob 
                 else:
                     if l_edges_in.get(node) or l_edges_out.get(node):
                         free_right_side[node] = node
-                    else:
+                    elif r_edges_in.get(node) or r_edges_out.get(node):
                         free_left_side[node] = node
+                    else:
+                        free_both[node] = node
 
 
                 # PWL constraints for node CN probabilities
@@ -116,6 +119,18 @@ def ilp(nodes, edges, coverages, alpha, beta, rlen_params, outfile, source_prob 
                     model.addConstr(C * x2[node] >= sink_right[node], "cheap_right_out_"+node)
                     model.addConstr(x1[node] + x2[node] == 1, "x_sum_"+node)
 
+                elif free_both.get(node):
+                    x1[node+"_1"] = model.addVar(vtype = GRB.INTEGER, lb = 0, ub = 1, name = "x1_1_"+node)
+                    x2[node+"_1"] = model.addVar(vtype = GRB.INTEGER, lb = 0, ub = 1, name = "x2_1_"+node)
+                    model.addConstr(C * x1[node+"_1"] >= source_right[node], "cheap_right_in_"+node)
+                    model.addConstr(C * x2[node+"_1"] >= sink_right[node], "cheap_right_out_"+node)
+                    model.addConstr(x1[node+"_1"] + x2[node+"_1"] == 1, "x_sum_1_"+node)
+                    x1[node+"_2"] = model.addVar(vtype = GRB.INTEGER, lb = 0, ub = 1, name = "x1_2_"+node)
+                    x2[node+"_2"] = model.addVar(vtype = GRB.INTEGER, lb = 0, ub = 1, name = "x2_2_"+node)
+                    model.addConstr(C * x1[node+"_2"] >= source_left[node], "cheap_left_in_"+node)
+                    model.addConstr(C * x2[node+"_2"] >= sink_left[node], "cheap_left_out_"+node)
+                    model.addConstr(x1[node+"_2"] + x2[node+"_2"] == 1, "x_sum_2_"+node)
+
 
             else:
                 concordance[node] = [-1]
@@ -130,6 +145,7 @@ def ilp(nodes, edges, coverages, alpha, beta, rlen_params, outfile, source_prob 
         model.setObjective(sum(p_cn[node] for node in nodes) + source_prob * sum(source_left[node] + source_right[node] + sink_left[node] + sink_right[node] for node in double_sides) +
                            sum(cheap_source*source_left[node] + source_prob*source_right[node] + cheap_source*sink_left[node] + source_prob*sink_right[node] for node in free_left_side) +
                            sum(source_prob*source_left[node] + cheap_source*source_right[node] + source_prob*sink_left[node] + cheap_source*sink_right[node] for node in free_right_side) +
+                           sum(cheap_source*source_left[node] + cheap_source*source_right[node] + cheap_source*sink_left[node] + cheap_source*sink_right[node] for node in free_both) +
                            sum(edge_flow[edges[e]] * min(0, ctp.edge_cov_pen(edges[e].sup_reads, alpha, edges[e].ovlp, rlen_params, cheap_source)) for e in edges), GRB.MAXIMIZE)
 
 
