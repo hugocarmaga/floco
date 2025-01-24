f71780dddff15c89aeadfa38d7a055d6a093c5e8
Timofey Prodanov <timofey.prodanov@gmail.com>
2025-01-24 23:49:16 +0100

    Change default epsilon

--- flow_main.py
+++ flow_main.py
@@ -35,7 +35,7 @@ def parse_arguments():
     parser.add_argument("-p", "--ploidy", type=int, default=2, help="Ploidy of the dataset. (default:%(default)s)")
     parser.add_argument("-S", "--super-prob", type=int, default=-10, help="Probability for using the super edges when there are other edges available. (default:%(default)s)")
     parser.add_argument("-s", "--cheap-prob", type=int, default=-2, help="Probability for using the super edges when there's no other edge available. (default:%(default)s)")
-    parser.add_argument("-e", "--epsilon", type=float, default=0.3, help="Epsilon value for adjusting CN0 counts to probabilities (default:%(default)s)")
+    parser.add_argument("-e", "--epsilon", type=float, default=0.02, help="Epsilon value for adjusting CN0 counts to probabilities (default:%(default)s)")
     parser.add_argument("-b", "--bin-size", default=100, type=int, help="Set the bin size to use for the NB parameters' estimation. (default:%(default)s)")
     parser.add_argument("-C", "--complexity", type=int, default=3, help="Model complexity (1-3): larger = slower and more accurate. (default: %(default)s)")
     parser.add_argument("-d", "--pickle", type=str, help="Pickle dump with the data.", required=False)
