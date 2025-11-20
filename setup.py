f437d5b6c58cbb4a8351906d9d4fcd4b4b195a1a
hugocarmaga <hugocarmaga@hotmail.com>
2025-11-20 15:57:11 +0100

    rechange package_dir in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=long_description,
     url='https://github.com/hugocarmaga/floco',
 
-    package_dir={'': 'src/floco'},
+    package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
     python_requires='>=3.10',
     install_requires=requirements,
